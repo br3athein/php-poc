bf329f32b86bd28d3a31c9541331fd1225369bfe
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-10-16 04:29:50 +0300

    [REM] Drop unused columns from database structure

--- db/initdb-entrypoint.d/00-initdb.sql
+++ db/initdb-entrypoint.d/00-initdb.sql
@@ -1,10 +1,7 @@
 CREATE TABLE `users` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(30) DEFAULT NULL,
-  `lastname` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
   `password_hash` varchar(60) NOT NULL,
-  `registration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `login` varchar(30) NOT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
