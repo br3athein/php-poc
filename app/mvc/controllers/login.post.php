af7dcabe7a29fac1a391a06f15b38ef922d8c73c
br3athein <br3athein@gmail.com>
2018-10-26 09:39:49 +0300

    Authenticate ppl

--- app/mvc/controllers/login.post.php
+++ app/mvc/controllers/login.post.php
@@ -1,25 +1,12 @@
 <?php
 
-require '../dbconn.php';
-$conn = establish_dbconn();
+require '../models/users.php';
 
-$search_login = $_POST['login'];
-$search_pwd = $_POST['pwd'];
+$givenLogin = $_POST['login'];
+$givenPassword = $_POST['pwd'];
 
-$query = 'SELECT login, password_hash FROM users WHERE login = ?';
-if ($stmt = $conn->prepare($query)) {
-    $stmt->bind_param('s', $search_login);
-    $stmt->execute();
-    $stmt->bind_result($user_login, $user_pwd_hash);
-    $stmt->fetch();
-
-    if ($user_login && password_verify($search_pwd, $user_pwd_hash)) {
-        session_start();
-        $_SESSION['login'] = $search_login;
-        $_SESSION['pwd_hash'] = $user_pwd_hash;
-        header('Location: insight.php');
-        die();
-    } else {
-        die('Login failed for ' . $search_login . ': wrong combination of credentials.');
-    }
+if (!$requestedUser = Users.search(login, $givenLogin)) {
+    throw AuthException('Username not found, noone to authenticate.');
 }
+
+$requestedUser->authenticate($givenPassword);
