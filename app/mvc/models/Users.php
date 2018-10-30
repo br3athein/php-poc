7de72f853dc45da5cd5bd2637583c72053ce2318
br3athein <br3athein@gmail.com>
2018-10-30 16:11:23 +0200

    [IMP] Implement `Users` model interface

--- app/mvc/models/Users.php
+++ app/mvc/models/Users.php
@@ -7,41 +7,89 @@
 
 namespace app\models;
 
-\Model::useConnection(\establishDatabaseConn(), 'mydb');
-
 class Users extends \app\lib\Model {
+
+    private $conn = null;
+
+    public function __construct()
+    {
+        // TODO: why not? Is that class needed?
+        $this->conn = \DatabaseConnection::establishDatabaseConnection();
+    }
+
     /**
      * Try to authenticate using given credentials.
      */
-    public function authenticate($pwd) {
-        if (password_verify($pwd, $this->password_hash)) {
-            $_SESSION['login'] = $this->login;
-            $_SESSION['pwd_hash'] = $this->password_hash;
-            return true;
+    public function authenticate($login, $pwd)
+    {
+        if ($this->verifyPwd($login, $pwd)) {
+            $_SESSION['login'] = $login;
+            // TODO: avoid storing hashes in session, replace w/ remember_token
+            $_SESSION['pwd_hash'] = $password_hash;
         }
-        throw new \orm\AuthException();
-    }
-    public function verifyPwdHash($pwdHash) {
-        if ($pwdHash !== $this->password_hash) {
-            throw new \orm\AuthException('Such hash mismatch. Wow.');
-        }
-        return;
+        throw new \AuthException();
     }
 
-    public static function getByLogin($login) {
-        return self::retrieveByField('login', $login, \orm\Model::FETCH_ONE);
+    public function verifyPwd($login, $pwd)
+    {
+        $sql = "SELECT `password_hash` FROM `users` WHERE `login` = ? LIMIT 1";
+        if ($stmt = $this->conn->prepare($sql)) {
+            $stmt->bind_param('s', $login);
+            $stmt->execute();
+            if ($res = $stmt->fetch_assoc()) {
+                if (password_verify($pwd, $res['password_hash'])) {
+                    return true;
+                }
+                throw new \AuthException('Such hash mismatch. Wow.');
+            }
+        }
     }
 
-    public function register($login, $pwd) {
+    public function checkLoginExists($login)
+    {
+        $query = "SELECT id FROM users WHERE login=? LIMIT 1";
+        if ($stmt = $this->conn->prepare($query)) {
+            $stmt->bind_param('s', $login);
+            $stmt->execute();
+            if ($stmt->fetch()) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public function register($login, $pwd)
+    {
         if ($this->getByLogin($login)) {
             throw new \orm\OrmException(
                 'FATAL: user "' . $login . '" exists already.'
             );
         }
 
-        $user = new Users;
-        $user->login = $login;
-        $user->password_hash = password_hash($pwd, PASSWORD_DEFAULT);
-        $user->save();
+        $sql = "INSERT INTO `users` (%s) VALUES (%s)";
+        $values = [
+            'login' => $login,
+            'password_hash' => password_hash($pwd, PASSWORD_DEFAULT),
+        ];
+
+        // compile statement - to be extracted, this is highly reusable
+        $fieldNames = $fieldValues = $fieldMarkers = $types = [];
+        foreach ($values as $fieldName => $fieldValue) {
+            $fieldNames[] = $fieldName;
+            $fieldMarkers[] = '?';
+            $fieldValues[] = $fieldValue;
+            // 'd' for doubles, 'i' for ints, 's' for the rest
+            $types[] = is_int($value) ? 'i' : is_double($value) ? 'd' : 's';
+        }
+        $sql = sprintf(
+            $sql, implode(', ', $fieldNames), implode(', ', $fieldMarkers)
+        );
+        call_user_func_array([$stmt, 'bind_param'], [implode($types), $fieldValues]);
+        // end compile statement
+
+        $stmt->execute();
+        if ($stmt->error) {
+            throw new \MyException('Unable to create user: ' . $stmt->error);
+        }
     }
 }
