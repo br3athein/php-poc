7a0cf0bc4f4788fdbcfe782b86c78265a649df3e
br3athein <br3athein@gmail.com>
2018-10-31 16:54:28 +0200

    [UI/UX] Reverse order of displayed blog posts
    so that the oldest stuff appears at the bottom of a page.

--- app/app/Http/Controllers/BlogController.php
+++ app/app/Http/Controllers/BlogController.php
@@ -13,7 +13,7 @@ class BlogController extends Controller
         return view(
             'blog', [
                 // TODO: no paging here
-                'posts' => BlogPost::with(['author'])->get(),
+                'posts' => BlogPost::with(['author'])->get()->reverse(),
             ]
         );
     }
